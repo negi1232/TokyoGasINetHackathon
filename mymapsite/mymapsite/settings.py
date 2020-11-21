1d180fb7940609c01689e265ad4c677a0d9af3f6
[negi] <[sakuryousuke1232@gmail.com]>
2020-11-21 02:53:00 -0800

    djangoを使用してgooglemapを表示させる

--- mymapsite/mymapsite/settings.py
+++ mymapsite/mymapsite/settings.py
@@ -38,7 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gmap.apps.GmapConfig',
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+LOGIN_REDIRECT_URL = '/gmap/api/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- mymapsite/mymapsite/urls.py
+++ mymapsite/mymapsite/urls.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
-from django.urls import include, path #include追加
+from gmap.models import Customer
+from django.urls import path
+from django.conf.urls import url, include
+from rest_framework import routers, serializers, viewsets
+
+class CustomerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ('name', 'address', 'lat', 'lng')
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+router = routers.DefaultRouter()
+router.register(r'customer', CustomerViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('gmap/', include('gmap.urls')), #追加
+    path('gmap/', include('gmap.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('gmap/api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
